2e34b44b2d7d5d3821e6de2666ec26599ce4adf1
Kristina047 <ch.drobot@gmail.com>
2024-11-12 16:54:22 +0300

    дз

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,17 @@ public class Main {
         byte b = -120;
         short c = 1040;
         long d = 69400L;
-        float g = 5647389f;
-        double k = 65736573d;
-        System.out.println("Значение переменной "+ a +" с типом "+ int +" равно "+ 4 +"");
+        float g = 564737f;
+        double k = 6573658d;
+        System.out.println("Значение переменной A с типом int равно " + a + " ");
+        System.out.println("Значение переменной B с типом byte равно " + b + " "  );
+        System.out.println("Значение переменной C с типом short равно "+c+" ");
+        System.out.println("Значение переменной D с типом long равно "+d+" ");
+        System.out.println("Значение переменной G с типом float равно "+g+" ");
+        System.out.println("Значение переменной K с типом double равно "+k+" ");
     }
+
+}
+
 
 
