9927bc1ba34d43bf37f18a448cec9f69194f1580
Kristina047 <ch.drobot@gmail.com>
2024-11-12 14:57:04 +0300

    дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        int a = 40560;
+        byte b = -120;
+        short c = 1040;
+        long d = 69400L;
+        float g = 5647389f;
+        double k = 65736573d;
+        System.out.println("Значение переменной "+ a +" с типом "+ int +" равно "+ 4 +"");
     }
-}
+
+
